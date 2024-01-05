ed5ed043a3b697dbc3985ece9f933e6ff9eb5e3e
Tejas Panchal <83456638+tejaspanchall@users.noreply.github.com>
2024-01-05 18:04:00 +0530

    yooo

---
+++ GreetingMsg.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class GreetingMsg {
+    public static void main(String[] args) {
+        String name;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        name = input.next();
+
+        System.out.println("Hello " + name + ", I'm lucky to call you a friend. Thanks for all of your support.");
+    }
+}
